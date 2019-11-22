e3154c2ab6e62d4b760efce83114fb5f78dcd987
Chris Pruitt <chris.pruitt15@gmail.com>
2019-11-22 14:09:13 -0500

    checkpoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
+import 'connectivity.dart';
+import 'package:after_layout/after_layout.dart';
 
-void main() => runApp(MaterialApp(home: FlutterKiosk()));
+void main() {
+  ConnectionStatusSingleton connectionStatus = ConnectionStatusSingleton.getInstance();
+  connectionStatus.initialize();
 
-class FlutterKiosk extends StatefulWidget {
-  @override
-  _FlutterKioskState createState() => _FlutterKioskState();
+  runApp(MaterialApp(home: PageOne()));
 }
 
-class _FlutterKioskState extends State<FlutterKiosk> {
-  Completer<WebViewController> _controller = Completer<WebViewController>();
-  final Set<String> _favorites = Set<String>();
+class PageOne extends StatefulWidget {
+  @override
+  _Page createState() => _Page('https://www.wikipedia.org/wiki/Kraken');
+}
+
+class PageTwo extends StatefulWidget {
+  @override
+  _PageTwo createState() => _PageTwo('https://www.wikipedia.org/wiki/dragon');
+}
+
+
+class _Page extends State<PageOne> with AfterLayoutMixin<PageOne> {
+
+  String url;
+  WebViewController _controller;
+
+  _Page(String url) {
+    this.url = url;
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 5000), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => PageTwo()),
+      );
+      setState(() => {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +49,46 @@ class _FlutterKioskState extends State<FlutterKiosk> {
         title: const Text('G2Lytics Inaugural Hackathon!'),
       ),
       body: WebView(
-        initialUrl: 'https://en.wikipedia.org/wiki/Kraken',
+        initialUrl: url,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller = webViewController;
+        }
+      )
+    );
+  }
+}
+
+class _PageTwo extends State<PageTwo> with AfterLayoutMixin<PageTwo> {
+
+  String url;
+  WebViewController _controller;
+
+  _PageTwo(String url) {
+    this.url = url;
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 5000), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => PageOne()),
+      );
+      setState(() => {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('G2Lytics Inaugural Hackathon!'),
+      ),
+      body: WebView(
+        initialUrl: url,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller = webViewController;
+        }
       )
     );
   }
