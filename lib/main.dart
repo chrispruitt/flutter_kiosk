507850dc3806e34cd1d6b51cac5a8787ebdd8ff5
chrispruitt <chris.pruitt15@gmail.com>
2019-12-02 07:40:17 -0600

    MVP
    Loads new URL in list on timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
+import 'connectivity.dart';
+import 'package:after_layout/after_layout.dart';
 
-void main() => runApp(MaterialApp(home: FlutterKiosk()));
+void main() {
+  ConnectionStatusSingleton connectionStatus = ConnectionStatusSingleton.getInstance();
+  connectionStatus.initialize();
 
-class FlutterKiosk extends StatefulWidget {
-  @override
-  _FlutterKioskState createState() => _FlutterKioskState();
+  runApp(MaterialApp(home: PageOne()));
 }
 
-class _FlutterKioskState extends State<FlutterKiosk> {
-  Completer<WebViewController> _controller = Completer<WebViewController>();
-  final Set<String> _favorites = Set<String>();
+class PageOne extends StatefulWidget {
+  @override
+  _Page createState() => _Page([
+    'https://status.g2lytics.com',
+    'https://www.wikipedia.org/wiki/Kraken',
+    'https://www.msn.com/en-us/weather/fullscreenmaps', 
+    'https://weather.com/weather/5day/l/d31b209091908e29d78cfb58bcb998952d09db7e10e650e3f7b2d93516bcab18',
+    ], 20);
+}
+
+class _Page extends State<PageOne> with AfterLayoutMixin<PageOne> {
+
+  int _pageIndex;
+  int _durationSeconds;
+  List<String> _urls;
+  WebViewController _controller;
+
+  _Page(List<String> urls, int durationSeconds) {
+    this._pageIndex = 0;
+    this._urls = urls;
+    this._durationSeconds = durationSeconds;
+  }
+
+  nextPage() {
+    _pageIndex++;
+    if(_pageIndex >= _urls.length) {
+      _pageIndex = 0;
+    }
+    Future.delayed(Duration(seconds: _durationSeconds), () {
+      _controller.loadUrl(_urls[_pageIndex]);
+      setState(() => {});
+      nextPage();
+    });
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    nextPage();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +58,11 @@ class _FlutterKioskState extends State<FlutterKiosk> {
         title: const Text('G2Lytics Inaugural Hackathon!'),
       ),
       body: WebView(
-        initialUrl: 'https://en.wikipedia.org/wiki/Kraken',
+        initialUrl: _urls[_pageIndex],
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller = webViewController;
+        }
       )
     );
   }
